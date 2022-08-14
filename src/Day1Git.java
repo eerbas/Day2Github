bad7b1a64f809ff512d9332a49dfb09ee031d7f0
Ertugrul Erbas <ertugrulerbas@gmail.com>
2022-08-14 21:10:55 +0200

    5.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,10 +1,6 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Bismillah Git 1. Deneme");
-        System.out.println("Bismillah Git 2. Deneme");
-        System.out.println("Bismillah Git 3. Deneme");
-        System.out.println("Bismillah Git 4. Deneme");
-
 
 
     }
